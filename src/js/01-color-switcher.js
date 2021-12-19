86dd94e07717604fe1b2d681bd80e88e182cbfb2
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-19 15:59:15 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,16 +1,22 @@
+// ================== take refs  ==================
+
 const refs = {
     startButton: document.querySelector('button[data-start]'),
     stopButton: document.querySelector('button[data-stop]'),
     bodyStyle: document.querySelector('body')
 };
 
+// ================== get color  ==================
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
+// ================== add listeners  ==================
 refs.startButton.addEventListener('click', onStartButton);
 refs.stopButton.addEventListener('click', onStopButton);
+
+// ================= start change color  =================
 
 function onStartButton() {
     timerId = setInterval(() => {
@@ -22,7 +28,9 @@ function onStartButton() {
     }, 1000);
 };
 
+// ================== stop change color   ==================
+
 function onStopButton() {
     clearInterval(timerId);
     refs.startButton.addEventListener('click', onStartButton);
-}
+};
