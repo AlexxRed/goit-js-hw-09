de817abe5f7fb0adb2cba3abaa5af41bf3ba75b2
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-20 14:14:00 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,9 +3,27 @@
 const refs = {
     startButton: document.querySelector('button[data-start]'),
     stopButton: document.querySelector('button[data-stop]'),
-    bodyStyle: document.querySelector('body')
+    bodyStyle: document.querySelector('body'),
+    allButtons: document.querySelectorAll("button")
 };
 
+// ================== basic style  ==================
+refs.stopButton.disabled = true;
+
+refs.bodyStyle.style.display = 'flex';
+refs.bodyStyle.style.justifyContent = 'center';
+refs.bodyStyle.style.marginTop = '400px';
+refs.bodyStyle.style.cursor = 'pointer'
+
+refs.allButtons.forEach(btn => {
+    btn.style.cursor = 'pointer'
+    btn.style.display = 'inline block';
+    btn.style.justifyContent = 'center';
+    btn.style.alignItems = 'center';
+    btn.style.padding = '15px 30px';
+    btn.style.borderRadius = '10px';
+    btn.style.marginLeft = '30px';
+})
 // ================== get color  ==================
 
 function getRandomHexColor() {
@@ -21,6 +39,8 @@ refs.stopButton.addEventListener('click', onStopButton);
 function onStartButton() {
     timerId = setInterval(() => {
         if (timerId) {
+            refs.stopButton.disabled = false;
+            refs.startButton.disabled = 'disabled';
             refs.startButton.removeEventListener('click', onStartButton);
         refs.bodyStyle.style.backgroundColor = getRandomHexColor();
         console.log(timerId);
@@ -31,6 +51,8 @@ function onStartButton() {
 // ================== stop change color   ==================
 
 function onStopButton() {
+    refs.startButton.disabled = false;
+    refs.stopButton.disabled = true;
     clearInterval(timerId);
     refs.startButton.addEventListener('click', onStartButton);
 };
