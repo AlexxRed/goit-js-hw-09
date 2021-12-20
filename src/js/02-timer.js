8005212a0de48565d5a0fefba30e5fe1a572834c
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-20 20:46:49 +0200

    summury

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,23 @@
+// ================== imports  ==================
+import flatpickr from "flatpickr"
+import 'flatpickr/dist/flatpickr.min.css';
+// ================== take refs  ==================
+
 const refs = {
     dateInput: document.querySelector('#datetime-picker'),
 
 };
 console.log(refs.dateInput);
+
+// ================== basic style  ==================
+// refs.dateInput.type = 'date';
+const flatpickrOptions = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+flatpickr(refs.dateInput, flatpickrOptions);
