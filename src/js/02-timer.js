c14b52848f21baca389b7a3700f98a4aa08360cd
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-23 00:47:42 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,21 +6,41 @@ import 'flatpickr/dist/flatpickr.min.css';
 const refs = {
     dateInput: document.querySelector('#datetime-picker'),
     startButton: document.querySelector('[data-start]'),
-  timer: document.querySelector('.timer'),
-  dateFields: document.querySelectorAll('field'),
+    timer: document.querySelector('.timer'),
+    dateFields: document.querySelectorAll('.field'),
     daysValue: document.querySelector('[data-days]'),
+    timerLable: document.querySelectorAll('.label')
 };
 console.log(refs.dateInput);
 console.log(refs.startButton);
+
 // ================== basic style  ==================
 // refs.dateInput.type = 'date';
 refs.startButton.disabled = true;
+refs.startButton.style.paddingTop = '7px';
+refs.startButton.style.paddingBottom = '6px';
+refs.startButton.style.fontSize = '24px';
+refs.startButton.style.textAlign = 'center'
 
+refs.dateInput.style.padding = '6px';
+refs.dateInput.style.fontSize = '25px';
+refs.dateInput.style.textAlign = 'center'
 refs.timer.style.display = 'flex';
+
 refs.dateFields.forEach(field => {
-  field.style.display = 'block'
-  field.style.justifyContent = 'flex-wrap';
+  field.style.marginRight = '20px';
+  field.style.marginTop = '20px'
+  field.style.fontSize = '25px';
+  field.style.textAlign = 'center'
+  field.style.fontWeight = '500'
 })
+
+refs.timerLable.forEach(lable => {
+  lable.style.display = 'block';
+})
+
+
+// ================== flatpickr init ==================
 
 const flatpickrOptions = {
   enableTime: true,
